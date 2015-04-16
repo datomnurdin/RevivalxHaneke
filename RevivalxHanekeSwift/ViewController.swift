d24199fb71ef2d7167ecf0370585ce55fc8519f0
Mohammad Nurdin <nurdinnorazanservices@gmail.com>
2015-04-16 17:18:00 +0800

    Final commit.

--- RevivalxHanekeSwift/ViewController.swift
+++ RevivalxHanekeSwift/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Haneke
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "https://graph.facebook.com/10204521055439574/picture?type=large")
+        //profilePhotoImage.contentMode = .ScaleAspectFill
+        imageView.hnk_setImageFromURL(url!)
     }
 
     override func didReceiveMemoryWarning() {
